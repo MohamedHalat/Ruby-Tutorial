4d8a8514a81c323356a16d1f1c6010856d5cb1bd
Mohamed Halat <dev@mohamedhalat.com>
2021-01-04 13:16:33 -0500

    Added more static pages and use yeild/provide

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/contact'
   get 'foo/bar'
   get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
   resources :microposts
   resources :users
   root 'users#index'
