a3c4df0e2c0a89ce29852afe3a7e155e2d90d889
Mohamed Halat <dev@mohamedhalat.com>
2021-01-04 11:08:53 -0500

    commit using commands

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    def hello
+        render html: "Hß∂ƒ∂ßƒ©ello World"
+    end
+    
+    def goodbye
+        render html: "Bye World"
+    end
 end
