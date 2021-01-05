d83f58d59c0c84e5c7ed6d098b5e012173b13562
Mohamed Halat <dev@mohamedhalat.com>
2021-01-05 11:29:25 -0500

    adding password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
-    has_many :microposts
-    validates :name, presence: true
-    validates :email, presence: true
+  before_save { email.downcase! }
+  has_many :microposts
+  validates :name, presence: true, length: { minimum: 0, maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { minimum: 0, maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  
+  has_secure_password
+  validates :password, length: { minimum: 6, maximum:128}
 end
