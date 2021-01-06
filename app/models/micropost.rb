0574b6ad5b33ca0bde5dcf14c2a13d4a26b74917
Mohamed Halat <dev@mohamedhalat.com>
2021-01-06 09:17:33 -0500

    images in microposts are now scalable

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,16 @@
 class Micropost < ApplicationRecord
     validates :content, length: {minimum:0, maximum:100}, presence: true
     validates :user, presence: true
+    validate :picture_size
     default_scope -> { order(created_at: :desc) }
     belongs_to :user
     mount_uploader :picture, PictureUploader
+
+    private
+
+    def picture_size
+        if picture.size > 5.megabytes
+            errors.add(:picture, "should be less than 5MB")
+        end
+    end
 end
